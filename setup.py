73586df067ecafccfd4fc5a7ae3c0e1a6786a3f3
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2015-03-24 17:12:33 +0000

    [setup.py] use setuptools instead of distutils (supports wheels)

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-from distutils.core import setup, Extension
-from distutils.command.build_ext import build_ext
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
 import platform
 
 
